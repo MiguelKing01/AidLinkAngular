6adcfc8509f29161390026edca8b5c159acf7493
MiguelKing01 <mgaleanoc@uniempresarial.edu.co>
2025-09-17 18:33:10 -0500

    InicioSesion

--- src/app/pages/inicio-sesion/inicio-sesion.ts
+++ src/app/pages/inicio-sesion/inicio-sesion.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { Footer } from '../../components/footer/footer';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Usuario, UsuarioService } from '../../services/usuario';
 
 @Component({
   selector: 'app-inicio-sesion',
-  imports: [Header, Footer, RouterLink],
+  imports: [Header, Footer, RouterLink, FormsModule],
   templateUrl: './inicio-sesion.html',
-  styleUrl: './inicio-sesion.css'
+  styleUrl: './inicio-sesion.css',
 })
 export class InicioSesion {
+  usuarios: Usuario[] = [];
 
+  correo: string = '';
+  contrasena: string = '';
+
+  constructor(private usuarioService: UsuarioService, private router: Router) {}
+
+  IniciarSesion() {
+    this.usuarioService.getUsuario().subscribe({
+      next: (data) => {
+        this.usuarios = data;
+
+        const encontrado = this.usuarios.find(
+          (u) => u.correo === this.correo && u.contrasena === this.contrasena
+        );
+
+        if (encontrado) {
+          this.router.navigate(['/inicio']);
+        } else {
+          const error = document.getElementById('error')
+          const msError = document.createElement('p');
+          msError.textContent="Correo o contraseña incorrectas";
+          error?.appendChild(msError);
+        }
+      },
+      error: (err) => {
+        console.error('Error al obtener usuarios', err);
+        alert('Error de conexión con el servidor');
+      },
+    });
+  }
 }
---
+++ src/app/services/usuario.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Usuario {
+  id: number;
+  nombre: String;
+  apellido: String;
+  correo: String;
+  contrasena: String;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioService {
+  private apiUrl = 'http://localhost:8080/Usuario';
+
+  constructor(private http: HttpClient) {}
+
+  getUsuario(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(`${this.apiUrl}/todos`);
+  }
+
+  crearUsuario(usuario: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>(`${this.apiUrl}/crear`, usuario);
+  }
+
+  getUsuarioId(id: number): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(`${this.apiUrl}/${id}`);
+  }
+
+  actualizarUsuario(usuario: Usuario): Observable<Usuario> {
+    return this.http.put<Usuario>(`${this.apiUrl}/actualizar`, usuario);
+  }
+}
