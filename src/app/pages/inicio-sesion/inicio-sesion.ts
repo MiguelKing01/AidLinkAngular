2772070e806e768f649cdadce6350cba59fe52ea
MiguelKing01 <mgaleanoc@uniempresarial.edu.co>
2025-09-21 17:53:43 -0500

    Validaciones

--- src/app/pages/inicio-sesion/inicio-sesion.ts
+++ src/app/pages/inicio-sesion/inicio-sesion.ts
@@ -2,40 +2,52 @@ import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { Footer } from '../../components/footer/footer';
 import { Router, RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Usuario, UsuarioService } from '../../services/usuario';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio-sesion',
-  imports: [Header, Footer, RouterLink, FormsModule],
+  imports: [Header, Footer, RouterLink, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './inicio-sesion.html',
   styleUrl: './inicio-sesion.css',
 })
 export class InicioSesion {
+
   usuarios: Usuario[] = [];
 
-  correo: string = '';
-  contrasena: string = '';
+  inicioSesion = new FormGroup({
+    correo: new FormControl('', [Validators.required, Validators.email]),
+    contrasena: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
 
   constructor(private usuarioService: UsuarioService, private router: Router) {}
 
   IniciarSesion() {
+    if (this.inicioSesion.invalid)
+      return; 
+
+    const { correo, contrasena } = this.inicioSesion.value;
+
     this.usuarioService.getUsuario().subscribe({
       next: (data) => {
         this.usuarios = data;
 
         const encontrado = this.usuarios.find(
-          (u) => u.correo === this.correo && u.contrasena === this.contrasena
+          (u) => u.correo === correo && u.contrasena === contrasena
         );
 
         if (encontrado) {
           this.usuarioService.setUsuarioId(encontrado.id!);
           this.router.navigate(['/inicio']);
         } else {
-          const error = document.getElementById('error')
-          const msError = document.createElement('p');
-          msError.textContent="Correo o contraseña incorrectas";
-          error?.appendChild(msError);
+          const error = document.getElementById('error');
+          if (error) {
+            error.innerHTML = ""; 
+            const msError = document.createElement('p');
+            msError.textContent = "Correo o contraseña incorrectas";
+            error.appendChild(msError);
+          }
         }
       },
       error: (err) => {
@@ -43,5 +55,13 @@ export class InicioSesion {
         alert('Error de conexión con el servidor');
       },
     });
+  }
+
+  get correoControl() {
+    return this.inicioSesion.get('correo');
+  }
+
+  get contrasenaControl() {
+    return this.inicioSesion.get('contrasena');
   }
 }
--- src/app/pages/registro/registro.ts
+++ src/app/pages/registro/registro.ts
@@ -3,33 +3,53 @@ import { Header } from '../../components/header/header';
 import { Footer } from '../../components/footer/footer';
 import { Router, RouterLink, RouterModule } from '@angular/router';
 import { Usuario, UsuarioService } from '../../services/usuario';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registro',
-  imports: [Header, Footer, RouterLink, FormsModule],
+  imports: [Header, Footer, RouterLink, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './registro.html',
   styleUrl: './registro.css',
 })
-export class Registro {
-  nuevoUsuario: Usuario = {
-    nombre: '',
-    apellido: '',
-    correo: '',
-    contrasena: '',
-  };
 
-  constructor(private usuarioService: UsuarioService, private router: Router) {}
+export class Registro {
+  registroForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private usuarioService: UsuarioService,
+    private router: Router
+  ) {
+
+    this.registroForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(2)]],
+      apellido: ['', [Validators.required, Validators.minLength(2)]],
+      correo: ['', [Validators.required, Validators.email]],
+      contrasena: ['', [Validators.required, Validators.minLength(6)]],
+      confirmarContrasena: ['', [Validators.required]],
+    }, {
+      validators: this.passwordsIguales 
+    });
+  }
+
+  passwordsIguales(form: FormGroup) {
+    const pass = form.get('contrasena')?.value;
+    const confirm = form.get('confirmarContrasena')?.value;
+    return pass === confirm ? null : { noCoinciden: true };
+  }
 
   registrarUsuario() {
-    this.usuarioService.crearUsuario(this.nuevoUsuario).subscribe({
-      next: () => {
-        this.router.navigate(['/ingresar']);
-      },
-      error: (err) => {
-        console.error('Error al registrar usuario', err);
-        alert('No se pudo registrar el usuario');
-      },
-    });
+    if (this.registroForm.valid) {
+      this.usuarioService.crearUsuario(this.registroForm.value).subscribe({
+        next: () => {
+          this.router.navigate(['/inicio/ingresar']);
+        },
+        error: (err) => {
+          console.error('Error al registrar usuario', err);
+          alert('No se pudo registrar el usuario');
+        },
+      });
+    }
   }
 }
